05d8b30144e3129d0e7106157c9e41048a0be145
Boris Stanojevic <borisstanojevicfkkt@gmail.com>
2022-07-19 17:21:09 +0000

    Refactor

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 // Assume credit card number numeric
 const isValid = (creditCardNumber) => {
-	if (isNullish(creditCardNumber))
-		throw Error("Credit card number cannot be null or undefined.");
+  if (isNullish(creditCardNumber))
+    throw Error("Credit card number cannot be null or undefined.");
 
-	const potentialCheckDigit = parseInt(creditCardNumber[creditCardNumber.length - 1]);
-	const payload = [...creditCardNumber.slice(0, -1)];
-	const checkDigit = calculateCheckDigit(payload);
+  const potentialCheckDigit = parseInt(creditCardNumber[creditCardNumber.length - 1]);
+  const payload = [...creditCardNumber.slice(0, -1)];
+  const checkDigit = calculateCheckDigit(payload);
 
-	return potentialCheckDigit === checkDigit;
+  return potentialCheckDigit === checkDigit;
 }
 
 const isNullish = (value) => value === null || value === undefined;
 
 const calculateCheckDigit = (payload) => {
-	let totalSum = 0;
+  let totalSum = 0;
 
-	let isDigitSupposedToBeMultiplied = true;
-	for (let i = payload.length - 1; i >= 0; i--) {
-		let digit = parseInt(payload[i], 10);
+  let isDigitSupposedToBeMultiplied = true;
+  for (let i = payload.length - 1; i >= 0; i--) {
+    let digit = parseInt(payload[i], 10);
 
-		let sum = digit;
+    let sum = digit;
 
-		if (isDigitSupposedToBeMultiplied)
-			sum = sumDigits(digit * 2);
+    if (isDigitSupposedToBeMultiplied)
+      sum = sumDigits(digit * 2);
 
-		totalSum += sum;
-		isDigitSupposedToBeMultiplied = !isDigitSupposedToBeMultiplied;
-	}
+    totalSum += sum;
+    isDigitSupposedToBeMultiplied = !isDigitSupposedToBeMultiplied;
+  }
 
-	return 10 - (totalSum % 10);
+  return 10 - (totalSum % 10);
 }
 
 const sumDigits = (number) => Math.floor(number / 10) + (number % 10);
